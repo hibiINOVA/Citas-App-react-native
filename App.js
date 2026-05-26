a8c78aee7b994fd1e0d3df0960d07f6be97a8304
hibiINOVA <conoplant2@gmail.com>
2026-05-26 10:37:26 -0600

    subir proyecto

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Pressable } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>
+        Administrador de Citas{' '}
+        <Text style={styles.tituloBold}>Veterinaria</Text>
+      </Text>
+
+      <Pressable style={styles.btnNuevaCita}>
+        <Text style={styles.btnTextoNuevaCita}>Nueva Cita</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F3F4F6',
+    flex: 1
   },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
+  },
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6D28D9',
+  },
+  btnNuevaCita: {
+    backgroundColor: '#6D28D9',
+    padding: 15,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10
+  },
+  btnTextoNuevaCita: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '900',
+    textTransform: 'uppercase'
+  },
+  noPacientes: {
+    marginTop: 40,
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: '600'
+  },
+  listado: {
+    marginTop: 50,
+    marginHorizontal: 30
+  }
 });
