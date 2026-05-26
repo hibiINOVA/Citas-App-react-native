7b0b798b203c284fb76e9843f7d78373b0671d7d
hibiINOVA <conoplant2@gmail.com>
2026-05-26 10:54:32 -0600

    clase 26 de mayo 2026

--- App.js
+++ App.js
@@ -1,17 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, Pressable } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Pressable,
+  Modal
+} from 'react-native';
 
 export default function App() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <SafeAreaView style={styles.container}>
+
       <Text style={styles.titulo}>
         Administrador de Citas{' '}
         <Text style={styles.tituloBold}>Veterinaria</Text>
       </Text>
 
-      <Pressable style={styles.btnNuevaCita}>
-        <Text style={styles.btnTextoNuevaCita}>Nueva Cita</Text>
+      <Modal
+        visible={modalVisible}
+        animationType='slide'
+      >
+        <Text style={styles.modalText}>Este es un Modal</Text>
+        <Pressable
+          onPress={() => setModalVisible(false)}
+          style={styles.btnNuevaCita}
+        >
+          <Text style={styles.btnTextoNuevaCita}>
+            Cerrar Modal
+          </Text>
+        </Pressable>
+      </Modal>
+
+      <Pressable
+        style={styles.btnNuevaCita}
+        onPress={() => setModalVisible(true)}
+        style ={styles.btnNuevaCita}
+      >
+        <Text style={styles.btnTextoNuevaCita}>
+          Nueva Cita
+        </Text>
       </Pressable>
+
     </SafeAreaView>
   );
 }
@@ -21,16 +53,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#F3F4F6',
     flex: 1
   },
+
   titulo: {
     textAlign: 'center',
     fontSize: 30,
     color: '#374151',
     fontWeight: '600'
   },
+
   tituloBold: {
     fontWeight: '900',
     color: '#6D28D9',
   },
+
   btnNuevaCita: {
     backgroundColor: '#6D28D9',
     padding: 15,
@@ -38,21 +73,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     borderRadius: 10
   },
+
   btnTextoNuevaCita: {
     textAlign: 'center',
     color: '#FFF',
     fontSize: 18,
     fontWeight: '900',
     textTransform: 'uppercase'
-  },
-  noPacientes: {
-    marginTop: 40,
-    textAlign: 'center',
-    fontSize: 24,
-    fontWeight: '600'
-  },
-  listado: {
-    marginTop: 50,
-    marginHorizontal: 30
   }
 });
